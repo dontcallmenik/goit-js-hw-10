f6083189e81e17ab0d942fdda919299e9365743e
dontcallmenik <114148391+dontcallmenik@users.noreply.github.com>
2026-06-08 20:13:02 +0000

    Deploying to gh-pages from @ dontcallmenik/goit-js-hw-10@d25e4f5a637a1678000a721d7646aa2f2d9edefc 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as r}from"./assets/vendor-BbbuE1sJ.js";const m=document.querySelector(".form");m.addEventListener("submit",t=>{t.preventDefault();const s=Number(t.target.delay.value),o=t.target.state.value;new Promise((e,i)=>{setTimeout(()=>{o==="fulfilled"?e(s):i(s)},s)}).then(e=>{r.success({message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{r.error({message:`❌ Rejected promise in ${e}ms`})})});
+//# sourceMappingURL=2-snackbar.js.map
