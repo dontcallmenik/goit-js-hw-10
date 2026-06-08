d25e4f5a637a1678000a721d7646aa2f2d9edefc
dontcallmenik <dontcallmenik@gmail.com>
2026-06-08 22:12:36 +0200

    add timer and promise tasks

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,33 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const delay = Number(e.target.delay.value);
+  const state = e.target.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+});
